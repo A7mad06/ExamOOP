e9e42cbc9d9d28dada16ba26520ce04241458603
Ahmad Bustany <ahmadbustany06@gmail.com>
2025-01-13 13:29:22 +0200

    Assignment

---
+++ Session05/Assignment/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Range<int> r = new Range<int>(5,10);
+            //Console.WriteLine(r.Legnth());
+            //Console.WriteLine(r.IsInRange(11));
+        }
+    }
+}
---
+++ Session05/Assignment/ConsoleApp1/ConsoleApp1/Range.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Range<T> where T : IComparable<T>
+    {
+        public T Min { get; set; }
+        public T Max { get; set; }
+        public Range(T _Min, T _Max)
+        {
+            Min = _Min;
+            Max = _Max;
+        }
+        public bool IsInRange(T Number)
+        {
+            if (Number.CompareTo(Min) >= 0 && Number.CompareTo(Max) <= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public T Legnth()
+        {
+            return ((dynamic)Max - (dynamic)Min);
+        }
+    }
+}
